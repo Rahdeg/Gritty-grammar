1a687babd867e7956b56560cc878246a6da82807
Rahdeg <walett95@gmail.com>
2022-11-19 06:28:54 +0100

    fix: fixed data

---
+++ src/data/data.js
@@ -0,0 +1,62 @@
+import IMG1 from '../images/Frame 1000002030 (1).png'
+import IMG2 from '../images/Frame 1000002030 (2).png'
+import IMG3 from '../images/Frame 1000002031 (1).png'
+import IMG4 from '../images/Frame 1000002049 (1).png'
+import IMG5 from '../images/Frame 1000002029.png'
+
+export const Values = [
+    {
+       id:1,
+       message:`Be the best being good is good, but nothing beats being the best. This is why our Al continues to offer its services for as long as you want. There is an improvement graph but no graduation because you don't graduate from being the best`,
+       
+    },
+    {
+       id:2,
+       message:`Determination: luck and innate talents are good attributes to possess, but studying centuries of humanity has shown time and again that it's only with determination can we achieve the best. Our weekly reports are designed to aid and improve your determination.`,
+      
+     },
+     {
+        id:3,
+        message:`Be current: language, just like technology, is an ever-evolving field. Our algorithm ensures that our library keeps up with the current usage of words and tenses.`,
+        
+     },
+     {
+        id:4,
+        message:`Community: at gritty grammar, we are trying to bring people closer, not just raise a group of fluent robots. This is why our Al uses everyday conversations to teach and correct so that you become familiar with talking to real people`,
+       
+      }
+    ]
+
+
+    export const Teams = [
+        {
+           id:1,
+           name:'Donald John',
+           imgsrc:IMG1,
+           role: 'General Managing Director'
+        },
+        {
+           id:2,
+           name:'Jennifer',
+           imgsrc:IMG2,
+           role: 'Head of Communication',
+         },
+         {
+            id:3,
+            name:'Victoria',
+            imgsrc:IMG3,
+            role: 'Head of Graphics'
+         },
+         {
+            id:4,
+            name:'Kenneth',
+            imgsrc:IMG4,
+            role: 'Head of Engineering',
+          },
+          {
+            id:5,
+            name:'Susan',
+            imgsrc:IMG5,
+            role: 'Head of Design',
+          }
+        ]
